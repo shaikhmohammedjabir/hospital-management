efa6671bfb32e593833d11e0b5ee33292b54546e
shaikhmohammedjabir <shaikhmohammedjabir.5226@gmail.com>
2021-06-23 21:16:32 +0530

    login authenticating function completed 90%

--- login.py
+++ login.py
@@ -3,7 +3,9 @@ from tkinter import Tk,Label,Button,Entry,PhotoImage
 from tkinter.messagebox import showinfo
 from sqlite3 import connect,OperationalError
 from datetime import datetime
+from hashlib import sha3_512
 from sys import stderr
+from admin import Admin
 
 class Login(Tk):
     def __init__(self):
@@ -18,21 +20,22 @@ class Login(Tk):
         time.grid(row=0,column=1,pady=30)
         #username
         Label(self,text="username",font=',13,').grid(row=1,column=0)
-        user_name=Entry(self)
+        user_name=Entry(self,highlightcolor='green')
         user_name.grid(row=1,column=1)
+        user_name.focus_set()
         #password
         Label(self, text="password", font=',13,').grid(row=2, column=0)
-        password=Entry(self,show='*')
+        password=Entry(self,show='*',highlightcolor='green')
         password.grid(row=2, column=1)
         #login button
         img=PhotoImage(file='icon/login.png')
         login=Button(self,text="login",image=img,activebackground='green')
         login.grid(row=3, column=1, sticky='E')
-        login.bind('<Button-1>',lambda x:self.authenticate(user_name,password))
-        login.bind('<Button-3>',lambda x: self.authenticate(user_name, password))
+        login.bind('<Button-1>',lambda x:self.authenticate(user_name.get(),password.get()))
+        login.bind('<Button-3>',lambda x: self.authenticate(user_name.get(), password.get()))
 
-        self.bind('<Return>',lambda x:self.authenticate(user_name,password))
-        self.bind('<KP_Enter>',lambda x:self.authenticate(user_name,password))
+        self.bind('<Return>',lambda x:self.authenticate(user_name.get(),password.get()))
+        self.bind('<KP_Enter>',lambda x:self.authenticate(user_name.get(),password.get()))
         self.update(time)
         self.bind('<Escape>',lambda x:self.destroy())
         self.mainloop()
@@ -42,10 +45,18 @@ class Login(Tk):
         time.after(1,lambda:self.update(time))
 
     def authenticate(self,user_name,user_password):
+        if user_name.lower()=='admin':
+            if sha3_512(bytes(user_password,encoding='utf-8',errors='strict')).hexdigest()=='8e4a8dcb32209514125ccdfe8c2837a2af7a7c74c39c28355113a02f14b47780d654b73d75db7e9232af2db200bbcc9c06e9accc7b461a65225540812359dc72':
+                self.destroy()
+                Admin()
+            else:
+                showinfo("authentication error","admin password mismatch")
+            return
+
         database = connect('database/hospital.db')
         cursor = database.cursor()
         try:
-            cursor.execute(f"select * from user where name='{user_name.get()}' and password='{user_password.get()}'")
+            cursor.execute(f"select * from user where name='{user_name}' and password='{user_password}'")
             user=cursor.fetchone()
             if user:
             #    return user[-1]
