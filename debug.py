37c449554eec51cbf145c874b51066e088b276da
shaikhmohammedjabir <shaikhmohammedjabir.5226@gmail.com>
2021-06-27 10:17:54 +0530

    useless file

--- debug.py
+++ debug.py
@@ -2,4 +2,7 @@
 #:todo write testing code here
 #create table user(sl integer primary key autoincrement,user_name varchar2(30) unique,password varchar2(80),role varchar2(16))
 
+import time
+
+print()
 
